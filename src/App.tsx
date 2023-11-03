8873ee7b7bb249ca752ba3c98a8fe22ec22cf5a2
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-11-03 17:00:25 +0700

    upate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import './App.css';
-import { AuthLayout } from './components/layouts/AuthLayout';
-import { LoginForm } from './components/molecules/LoginForm';
-import { ImagesWithText } from './components/molecules/ImagesWithText';
+import { Login } from './components/templates/Login';
+import { useRecoilValue } from 'recoil';
+import { authScreenAtom } from './states/authAtoms';
 function App() {
+  const authScreenState = useRecoilValue(authScreenAtom);
+  console.log(authScreenState);
   return (
     <>
-      <AuthLayout>
-        <section className="max-w-[400px] mx-auto text-center 2xl:absolute">
-          <p className="text-base-semibold text-center text-light-1 px-10 mb-4">
-            Log in with your Instagram account
-          </p>
-          <LoginForm />
-          <p className="text-[#777777] text-[15px] mb-4">Forgot password?</p>
-          <div className="flex items-center text-[#323333] justify-center mb-5">
-            <p className="after:content-[''] flex-1 border border-b-0 border-[#323333]"></p>
-            <p className="text-center text-[#777777] mx-4">or</p>
-            <p className="before:content-[''] flex-1 border border-b-0 border-[#323333]"></p>
-          </div>
-          <ImagesWithText />
-        </section>
-      </AuthLayout>
+      <Login></Login>
     </>
   );
 }
--- src/components/molecules/LoginForm/LoginForm.tsx
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -3,24 +3,22 @@ import { Input } from '../../atoms/Input';
 import { Link } from 'react-router-dom';
 export const LoginForm = () => {
   return (
-    <div className="flex flex-col w-full mb-5">
-      <form action="">
-        <Input
-          name="email"
-          placeholder="Username, phone or email"
-          className="authen-login_input"
-        />
-        <Input
-          name="Password"
-          placeholder="Password"
-          className="authen-login_input"
-        />
-        <Button className="w-full bg-white py-6 ">
-          <Link to="/login" className="text-[#9f9f9f]">
-            Log in
-          </Link>
-        </Button>
-      </form>
-    </div>
+    <form className="flex flex-col w-full mb-5">
+      <Input
+        name="email"
+        placeholder="Username, phone or email"
+        className="authen-login_input"
+      />
+      <Input
+        name="password"
+        placeholder="Password"
+        className="authen-login_input"
+      />
+      <Button className="w-full bg-white py-6 ">
+        <Link to="/login" className="text-[#9f9f9f]">
+          Log in
+        </Link>
+      </Button>
+    </form>
   );
 };
