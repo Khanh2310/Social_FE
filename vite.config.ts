320a0bb56e5a19ad59c6e71f43e68d3103171280
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-11-06 08:48:45 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+
+    // get rid CRUD
+
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
