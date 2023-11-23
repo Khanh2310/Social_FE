c7350b54e52da3d8a5eceeccd68cd8568f21d593
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-11-23 10:16:15 +0700

    push

--- src/components/layouts/BasicLayout/BasicLayout.tsx
+++ src/components/layouts/BasicLayout/BasicLayout.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Topbar } from '../../molecules/Topbar';
 import { LeftSideBar } from '../../molecules/LeftSideBar';
 import { RightSideBar } from '../../molecules/RightSideBar';
 import { Bottombar } from '../../molecules/Bottombar';
-
+import { useRecoilValue } from 'recoil';
+import { userAtoms } from '../../../states/authAtoms';
 export const BasicLayout = () => {
+  const user = useRecoilValue(userAtoms);
+  console.log(user);
+  if (!user) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
   return (
     <>
       <Topbar />
