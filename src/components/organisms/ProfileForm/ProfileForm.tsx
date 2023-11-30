633d0b363d5875a0972a5066c1b419124ece9236
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-11-30 17:11:07 +0700

    update

---
+++ src/components/organisms/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,11 @@
+export const ProfileForm = () => {
+  return (
+    <div className="w-[800px] mx-auto bg-dark-4">
+      <div className="flex items-center justify-between">
+        <button className=""></button>
+        <div className="text-white">Edit Profile</div>
+        <button className=""></button>
+      </div>
+    </div>
+  );
+};
--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -4,11 +4,13 @@ import { BasicLayout } from '../components/layouts/BasicLayout';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { Home } from '../components/templates/Home';
+import { ProfileForm } from '../components/organisms/ProfileForm';
 export const RouterConfig: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="profile" element={<ProfileForm />} />
           <Route path="/" element={<BasicLayout />}>
             <Route index element={<Home />} />
           </Route>
